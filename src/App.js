f3718fa824653ac5d2111922333a5c36a8db3c87
Ray Nimalan <raymondnimalan@gmail.com>
2023-05-03 21:05:25 -0400

    feat: added agents components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+import PlayerCard from './Components/PlayerCard';
+import NavBar from './Components/NavBar';
+import AgentsCard from './Components/AgentsCard';
 
-function App() {
-  async function getStuff() {
+const App = () => {
+  const [agents, setAgents] = useState([]);
+
+  useEffect(() => {
+    getAgents();
+    console.log('agents from useEffect', agents);
+  }, []);
+
+  async function getAgents() {
     try {
       console.log('hello from fetch request');
-      const response = await axios.get('http://localhost:7653/stuff');
-      console.log('response', response);
+      const response = await axios.get('http://localhost:7653/agents');
+      const characters = await response.data.characters;
+      console.log('response characters', characters);
+      setAgents(characters);
       return response;
     } catch (error) {
       console.log('error from axios request to backend', error);
     }
   }
-
   return (
     <div className='App'>
+      <NavBar />
       <header className='App-header'>
+        <PlayerCard />
+        <AgentsCard agents={agents} setAgents={setAgents} />
         <img src={logo} className='App-logo' alt='logo' />
-        <button onClick={() => getStuff()}>Hello</button>
+        <button>Hello</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -34,6 +48,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
