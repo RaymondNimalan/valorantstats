f4fbd8cf86156629db87cf4842511872cff1e5a6
Ray Nimalan <raymondnimalan@gmail.com>
2023-05-15 18:32:11 -0400

    feat: map animations with images

--- src/Components/MapCard.js
+++ src/Components/MapCard.js
@@ -1,30 +1,58 @@
 import React from 'react';
+import mapInfoData from './Data/mapInfo';
+import styled, { css } from 'styled-components';
 
 const MapCard = ({ maps, setMaps }) => {
   console.log('from child comp maps', maps);
-  const MapContainerStyle = {
-    display: 'flex',
-    flexDirection: 'row',
-    flexWrap: 'wrap',
-    justifyContent: 'space-evenly',
-  };
-  const MapStyle = {
-    height: '100px',
-    width: '100px',
-  };
-  const Title = {};
+
+  const MapContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+    flex-wrap: wrap;
+    justify-content: space-between;
+  `;
+
+  const Map = styled.div`
+    display: flex;
+    flex-direction: column;
+    @keyframes puff-in-center {
+      0% {
+        transform: scale(2);
+        filter: blur(4px);
+        opacity: 0;
+      }
+      100% {
+        transform: scale(1);
+        filter: blur(0px);
+        opacity: 1;
+      }
+    }
+    animation: puff-in-center 0.7s cubic-bezier(0.47, 0, 0.745, 0.715) both;
+    img {
+      justify-content: center;
+      max-height: 200px;
+    }
+  `;
+  const Title = styled.div`
+    display: flex;
+    font-family: 'Bebas Neue, san serif';
+    font-weight: bold;
+    font-size: 24px;
+    padding: 12px;
+
+    justify-content: center;
+  `;
+
+  console.log(mapInfoData);
   return (
-    <div>
-      <div>Maps</div>
-      <div style={MapContainerStyle}>
-        {maps.map((map, index) => (
-          <div style={MapStyle}>
-            <div>{map.name}</div>
-            <div>Map Image</div>
-          </div>
-        ))}
-      </div>
-    </div>
+    <MapContainer id='maps'>
+      {maps.map((map, index) => (
+        <Map key={index}>
+          <Title>{`${map.name}`}</Title>
+          <img src={mapInfoData[`${map.name}`].image} alt='map' />
+        </Map>
+      ))}
+    </MapContainer>
   );
 };
 
