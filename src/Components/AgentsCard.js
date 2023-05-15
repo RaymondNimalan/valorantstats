d31bd0b5799b2a9f594ef262ba8083c7865ed6ac
Ray Nimalan <raymondnimalan@gmail.com>
2023-05-15 18:30:31 -0400

    fix: styling with styled-components

--- src/Components/AgentsCard.js
+++ src/Components/AgentsCard.js
@@ -1,29 +1,72 @@
 import { useState, useEffect } from 'react';
 import valorantAgents from './Data/agentDescriptions';
+import styled from 'styled-components';
+
+const AgentsContainer = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+`;
+
+const AgentCard = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: row;
+`;
+
+const AgentImage = styled.img`
+  display: flex;
+  max-height: 500px;
+  max-width: 500px;
+  height: 100%;
+  width: 100%;
+  transform: ${(props) =>
+    props.animateimage ? 'translateY(0)' : 'translateY(100px)'};
+  opacity: ${(props) => (props.animateimage ? 1 : 0)};
+  transition: transform 2s ease-in-out, opacity 2s ease-in-out;
+`;
+
+const AgentInfo = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  justify-content: center;
+  padding: 20px;
+  background-color: #f7f7f7;
+  border-radius: 10px;
+  box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.1);
+  max-width: 400px;
+  color: #282c34;
+  font-family: 'Bebas Neue';
+`;
+
+const Title = styled.div`
+  font-size: 32px;
+  font-weight: bold;
+`;
+
+const Header = styled.div`
+  font-size: 18px;
+  font-weight: bold;
+  margin: 12px 0 0 0;
+`;
+
+const Selector = styled.div`
+  max-height: 25px;
+`;
 
 const AgentsCard = ({ agents, setAgents }) => {
-  const [currentAgent, setCurrentAgent] = useState('Gekko');
-  const [animateImage, setAnimateImage] = useState(true);
+  const [currentAgent, setCurrentAgent] = useState('');
+  const [animateImage, setAnimateImage] = useState(false);
 
-  const AgentInfo = {
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'space-around',
-  };
-  const SelectorStyle = {
-    maxHeight: '25px',
-  };
-
-  const ImageStyle = {
-    display: 'flex',
-    maxHeight: '500px',
-    maxWidth: '500px',
-    height: '100%',
-    width: '100%',
-    padding: '10px',
-    transform: animateImage ? 'translateY(0)' : 'translateY(100px)',
-    opacity: animateImage ? 1 : 0,
-    transition: 'transform 2s ease-in-out, opacity 2s ease-in-out',
+  const handleSubmit = (newAgent) => {
+    console.log('animateImage1', animateImage);
+    setAnimateImage(!animateImage);
+    setTimeout(() => {
+      console.log('animateImage2', animateImage);
+      setCurrentAgent(newAgent);
+      setAnimateImage(true);
+    }, 2000);
   };
 
   useEffect(() => {
@@ -32,32 +75,10 @@ const AgentsCard = ({ agents, setAgents }) => {
     //setAnimateImage(!animateImage);
   }, [currentAgent]);
 
-  const handleSubmit = (newAgent) => {
-    console.log('animateImage1', animateImage);
-    setAnimateImage(!animateImage);
-    console.log('animateImage2', animateImage);
-    setTimeout(() => {
-      setCurrentAgent(newAgent);
-      setAnimateImage(true);
-    }, 2000);
-  };
-
-  console.log('child comp', agents);
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        maxHeight: '500px',
-        height: '100%',
-      }}
-    >
-      <div>Agents</div>
+    <AgentsContainer>
       <button onClick={() => setAnimateImage(!animateImage)}></button>
-      <select
-        style={SelectorStyle}
-        onChange={(e) => handleSubmit(e.target.value)}
-      >
+      <select onChange={(e) => handleSubmit(e.target.value)}>
         {agents.map((agent, index) => (
           <option key={index} value={agent.name}>
             {agent.name}
@@ -65,29 +86,62 @@ const AgentsCard = ({ agents, setAgents }) => {
         ))}
       </select>
       {currentAgent ? (
-        <div
-          style={{
-            display: 'flex',
-            flexDirection: 'row',
-          }}
-        >
-          <img
+        <AgentCard>
+          <AgentImage
+            animateimage={animateImage}
             src={valorantAgents[`${currentAgent}`].image}
-            alt=''
-            style={ImageStyle}
+            alt='agent'
           />
-          <div style={AgentInfo}>
-            <div>{currentAgent}</div>
-            <div>{valorantAgents[`${currentAgent}`].role}</div>
-            <div>{valorantAgents[`${currentAgent}`].bio}</div>
-            <div>{valorantAgents[`${currentAgent}`].abilities}</div>
-          </div>
-        </div>
+          <AgentInfo>
+            <Title>{currentAgent}</Title>
+            <Header>##ROLE</Header>
+            <p>{valorantAgents[`${currentAgent}`].role}</p>
+            <Header>##BIO</Header>
+            <p>{valorantAgents[`${currentAgent}`].bio}</p>
+            <Header>##ABILITIES</Header>
+            <p>{valorantAgents[`${currentAgent}`].abilities}</p>
+          </AgentInfo>
+        </AgentCard>
       ) : (
         <div style={{ height: '500px' }}>Choose an Agent!</div>
       )}
-    </div>
+      ;
+    </AgentsContainer>
   );
 };
+
+// const AgentInfo = {
+//   display: 'flex',
+//   width: '50%',
+//   flexDirection: 'column',
+//   justifyContent: 'center',
+//   // padding: '16px',
+//   // margin: '0 0 16px',
+//   // flexWrap: 'wrap',
+//   padding: '20px',
+//   backgroundColor: '#f7f7f7',
+//   borderRadius: '10px',
+//   boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.1)',
+//   maxWidth: '400px',
+//   //margin: '20px',
+//   color: '#282c34',
+// };
+
+// const ImageStyle = {
+//   display: 'flex',
+//   maxHeight: '500px',
+//   maxWidth: '500px',
+//   height: '100%',
+//   width: '100%',
+//   //padding: '10px',
+//   transform: animateImage ? 'translateY(0)' : 'translateY(100px)',
+//   opacity: animateImage ? 1 : 0,
+//   transition: 'transform 2s ease-in-out, opacity 2s ease-in-out',
+
+// useEffect(() => {
+//   console.log('currentAgent', currentAgent);
+//   //console.log('animateImage', animateImage);
+//   //setAnimateImage(!animateImage);
+// }, [currentAgent]);
 
 export default AgentsCard;
