ca8eb7099a4cff7ceea6dff8c772d71065826b3d
Ray Nimalan <raymondnimalan@gmail.com>
2023-05-24 16:57:05 -0400

    feat: routed links with styles

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,32 +1,66 @@
 import React from 'react';
+//import { Link } from 'react-scroll';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const NavContainer = styled.div`
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  z-index: 100;
+`;
+
+const Nav = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-content: center;
+  height: 75px;
+  background-color: white;
+  border-radius: 16px;
+  opacity: 0.5;
+  align-items: center;
+  margin: 24px;
+  font-family: 'Bebas Neue';
+  font-size: 24px;
+  text-shadow: 2px 2px 4px #ffffff;
+`;
+
+const Title = styled(Link)`
+  display: flex;
+  align-items: flex-start;
+  padding: 25px;
+`;
+
+const NavLinks = styled.div`
+  display: flex;
+  width: 25%;
+  align-items: flex-end;
+  flex-direction: row;
+  justify-content: space-evenly;
+  padding: 12px;
+`;
+
+const Links = styled(Link)`
+  display: flex;
+`;
 
 const NavBar = () => {
-  const menuStyle = {
-    padding: '25px',
-  };
+  //const Title = styled.div``;
 
   return (
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        height: '75px',
-      }}
-    >
-      <div style={{ padding: '25px' }}>Title</div>
-      <div
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-around',
-        }}
-      >
-        <div style={menuStyle}>Weapons</div>
-        <div style={menuStyle}>Agents</div>
-        <div style={menuStyle}>Maps</div>
-      </div>
-    </div>
+    <NavContainer>
+      <Nav>
+        <Title to='/' style={{ padding: '25px' }}>
+          Home
+        </Title>
+        {/* <Link to='/agents'>Agents</Link> */}
+        <NavLinks>
+          <Links to='/maps'>Maps</Links>
+          <Links to='/agents'>Agents</Links>
+        </NavLinks>
+      </Nav>
+    </NavContainer>
   );
 };
 
