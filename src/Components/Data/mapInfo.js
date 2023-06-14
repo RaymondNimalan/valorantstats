0d8ce332b404a195b7d54be728eef8a90c6080ca
Ray Nimalan <raymondnimalan@gmail.com>
2023-06-14 13:32:50 -0400

    fix: update map data

---
+++ src/Components/Data/mapInfo.js
@@ -0,0 +1,48 @@
+const mapInfoData = {
+  Ascent: {
+    description: '',
+    image: '/images/maps/Ascent_minimap.png',
+    inRotation: 'true',
+  },
+  Bind: {
+    description: '',
+    image: '/images/maps/Bind_minimap.png',
+    inRotation: 'true',
+  },
+  Haven: {
+    description: '',
+    image: '/images/maps/Haven_minimap.png',
+    inRotation: 'true',
+  },
+  Fracture: {
+    description: '',
+    image: '/images/maps/Fracture_minimap.png',
+    inRotation: 'true',
+  },
+  Pearl: {
+    description: '',
+    image: '/images/maps/Pearl_minimap.png',
+    inRotation: 'true',
+  },
+  Lotus: {
+    description: '',
+    image: '/images/maps/Lotus_minimap.png',
+    inRotation: 'true',
+  },
+  Split: {
+    description: '',
+    image: '/images/maps/Split_minimap.png',
+    inRotation: 'true',
+  },
+  Icebox: {
+    description: '',
+    image: '/images/maps/Icebox_minimap.png',
+    inRotation: 'false',
+  },
+  Breeze: {
+    description: '',
+    image: '/images/maps/Breeze_minimap.png',
+    inRotation: 'false',
+  },
+};
+export default mapInfoData;
