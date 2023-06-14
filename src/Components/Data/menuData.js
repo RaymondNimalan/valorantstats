5938a78d2444bc6e8196fe2a007f3f753f611dc8
Ray Nimalan <raymondnimalan@gmail.com>
2023-06-14 13:33:01 -0400

    fix: update menu data

---
+++ src/Components/Data/menuData.js
@@ -0,0 +1,6 @@
+export const menuData = [
+  { title: 'Home', link: 'home', offset: 0 },
+  { title: 'Agents', link: 'agents', offset: -50 },
+  { title: 'Maps', link: 'maps', offset: -50 },
+  { title: 'Weapons', link: 'weapons', offset: -50 },
+];
